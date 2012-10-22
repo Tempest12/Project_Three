9d68359c706e2959ee19f0a5529d1b4d62e19280
Tempest12 <alexis.solaris@gmail.com>
2012-10-22 11:05:28 -0400

    Fixed Camera Bug added basic spraying capabilities

--- 6491_Project_3/RenderWindow.cpp
+++ 6491_Project_3/RenderWindow.cpp
@@ -7,13 +7,24 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <stdlib.h>
 
 #include "RenderWindow.hpp"
 #include "Core.hpp"
 #include "Config.hpp"
 #include "Sphere.hpp"
 
-std::list<Sphere>* sphereList;
+std::vector<Sphere>* sphereList;
+
+float diskOneDistance;
+float diskOneRadius;
+
+float diskTwoDistance;
+float diskTwoRadius;
+
+int spawnCount;
+float spawnRadius;
+float spawnSpeed;
 
 RenderWindow::RenderWindow() : Fl_Gl_Window(0, 0, "")
 {
@@ -31,14 +42,37 @@ void RenderWindow::initGraphcs()
 {
 	glClearColor(convertSettingToFloat("colours", "bg_colour_r"), convertSettingToFloat("colours", "bg_colour_g"), convertSettingToFloat("colours", "bg_colour_b"), convertSettingToFloat("colours", "bg_colour_a"));
 
-	sphereList = new std::list<Sphere>();
+	sphereList = new std::vector<Sphere>(5);
+
+	diskOneDistance = convertSettingToFloat("spawn", "spawn_radius");
+	diskOneRadius = convertSettingToFloat("spawn", "disk_one_radius");
+
+	diskTwoDistance = convertSettingToFloat("spawn", "diks_two_distance");
+	diskTwoRadius = convertSettingToFloat("spawn", "disk_two_radius");
+    
+	spawnCount = convertSettingToFloat("spawn", "spawn_count");
+	spawnRadius = convertSettingToFloat("spawn", "spawn_radius");
+	spawnSpeed = convertSettingToFloat("spawn", "spawn_speed");
 }
 
-int hi = 0;
+void RenderWindow::update(void)
+{
+	//Calculates the new position of the camera if it has moved
+	camera -> calculateCamera();
+	gluLookAt(camera -> position -> x, camera -> position -> y, camera -> position->z, camera -> focusPoint -> x, camera -> focusPoint -> y, camera -> focusPoint -> z, camera -> upVector -> x, camera -> upVector -> y, camera -> upVector -> z);
+
+	//Do Update Call on each sphere.
+	for(int index = 0; index < sphereList -> size(); index++)
+	{
+		(*sphereList)[index].update();		
+	}
+
+	//Do Coliision Detection
+}
 
 void RenderWindow::draw()
 {
-	update();
+	
 
 	glEnable(GL_DEPTH_TEST);
 
@@ -48,34 +82,33 @@ void RenderWindow::draw()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	//std::cout << "Yes" << std::endl;
-	camera -> calculateCamera();
-	gluLookAt(camera -> position -> x, camera -> position -> y, camera -> position->z, camera -> focusPoint -> x, camera -> focusPoint -> y, camera -> focusPoint -> z, camera -> upVector -> x, camera -> upVector -> y, camera -> upVector -> z);
-
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-	//Draw Spheres
-    //for(int index = 0; index <
+	update();
 
-	//Test spheres
+	//Draw Spheres
+    for(int index = 0; index < sphereList -> size(); index++)
+	{
+		(*sphereList)[index].draw();
+	}
+
+
+
 	glColor3f(0.0f, 0.0f, 0.8f);
 
-
-	glPushMatrix();
-	    //glTranslatef(0.0f, 0.0f, hi++);
-		glutWireSphere(1.0f, 25, 25); 
-	glPopMatrix();
-
-	//this -> redraw();
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glutWireSphere(1.0f, 15, 15);
 }
 
 int RenderWindow::handle(int event)
 {
-	//std::cout << "Handling input Event: " << event << std::endl;
-
 	switch(event)
 	{
 		case FL_PUSH: //Mouse Down
+			//std::cout << "Shout out to my PEEPS!!!" << std::endl;
+
+			//Calling Sphere Spawn
+			spawnSpheres();
 
 			break;
 
@@ -109,6 +142,7 @@ int RenderWindow::handle(int event)
 					{
 						camera -> changeVerticalAngle(false);
 					}
+					break; 
 
 				case 'd':
 					//std::cout << 'd' << std::endl;
@@ -141,5 +175,43 @@ int RenderWindow::handle(int event)
 			break;
 	}
 
+    
+
 	return 0;
+}
+
+void RenderWindow::spawnSpheres()
+{
+	//std::cout << "Count is: " << spawnCount << std::endl;
+
+	for(int count = 0; count < spawnCount; count++)
+	{
+		Sphere* temp;
+
+		//Determine Starting Location
+		float spawnDistance = (float)rand() / (float)RAND_MAX;
+		float spawnAngle = (float)rand() / (float)RAND_MAX;
+
+		Vector3f* directionVector = MyVector::subtract(camera -> position, camera -> focusPoint);
+		directionVector -> normalize();
+
+		Vector3f* diskOnePosition = MyVector::scale(directionVector, camera -> getDistance() - diskOneRadius);
+		Vector3f* diskTwoPosition = MyVector::scale(directionVector, camera -> getDistance() - diskOneRadius - diskTwoRadius);
+
+		Vector3f* otherDirectionVector = new Vector(
+
+		Vector3f* startVelocity = MyVector::subtract(diskTwoPosition, diskOnePosition);
+		startVelocity -> normalize();
+		startVelocity -> scale(spawnSpeed);
+
+		temp = new Sphere(diskOnePosition, startVelocity, spawnRadius);
+
+		//Add to list and let's move on
+		sphereList ->push_back(temp);
+
+		delete directionVector; 
+		delete diskOnePosition;
+		delete diskTwoPosition;
+		delete startVelocity;
+	}
 }
